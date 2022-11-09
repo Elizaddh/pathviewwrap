8baae4e026b1037f6a76ce62cfc05971b6eb102d
DEliza <32403146+Elizaddh@users.noreply.github.com>
2022-11-09 17:59:40 -0500

    Update path_rna_fastp.R

--- R/path_rna_fastp.R
+++ R/path_rna_fastp.R
@@ -1,6 +1,8 @@
 #run fastp
 #run fastp
-run_fastp <-function(samplename){
+run_fastp <-function(samplename, seq_tech){
+  
+  if (seq_tech == "PacBio" | seq_tech == "Nanopore" ){
 
   if (endness=="PE"){
 
@@ -11,9 +13,23 @@ run_fastp <-function(samplename){
 
 
   }
+    } else {
+    if (endness=="PE"){
+
+    cmd <- paste0("fastp -i " ,file.path(fq.dir , "samplename_to_sed_1.fastq") , " -I ",  file.path(fq.dir , "samplename_to_sed_2.fastq"), " -o ",  file.path(trim.dir , "samplename_to_sed_paired_1.fastq"), " -O ", file.path(trim.dir , "samplename_to_sed_paired_2.fastq"),   "-h samplename_to_sed.html", "-j samplename_to_sed.json")
+
+  } else {
+    cmd <- paste0("fastp -i ", file.path(fq.dir , "samplename_to_sed.fastq"), " -o ",  file.path(trim.dir , "samplename_to_sed_paired.fastq"), " -h samplename_to_sed.html", "-j samplename_to_sed.json")
+
+}
+    
+    
+    
+    
   cmd <- stringr::str_replace_all(cmd, "samplename_to_sed", samplename)
   print(cmd)
   system(cmd)
+    
 }
 
 
